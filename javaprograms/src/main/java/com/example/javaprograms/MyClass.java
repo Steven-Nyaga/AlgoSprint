2515b145a2f1de9da4050185b222b8f560187498
Steven-Nyaga <37731180+Steven-Nyaga@users.noreply.github.com>
2021-03-05 23:02:29 +0300

    Binary trees

--- javaprograms/src/main/java/com/example/javaprograms/MyClass.java
+++ javaprograms/src/main/java/com/example/javaprograms/MyClass.java
@@ -1,5 +1,7 @@
 package com.example.javaprograms;
 
+import java.util.ArrayList;
+
 public class MyClass {
     public static void main (String [] args){
         Tree tree = new Tree();
@@ -10,7 +12,52 @@ public class MyClass {
         tree.insert(6);
         tree.insert(8);
         tree.insert(10);
-        tree.postOrderTraverse();
-        System.out.println(tree.find(10));
+
+        Tree tree2 = new Tree();
+        tree2.insert(7);
+        tree2.insert(4);
+        tree2.insert(9);
+        tree2.insert(1);
+        tree2.insert(6);
+        tree2.insert(8);
+        tree2.insert(10);
+
+        System.out.println(tree.getAncestors(6));
+
+//        tree.swapRoot();
+//        ArrayList<Integer> list = new ArrayList<>();
+//        list = tree.getNodesAtDistance(2);
+//        for(int value : list)
+//            System.out.println(value);
+//        System.out.println(tree.isBinarySearchTree());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        String military = String.format("%02d:%02d:%02d", 7,25,15);
+//        String str1 = String.format("%d", 10);          // Integer value
+//        String str2 = String.format("%s", "Amar Singh"); // String value
+//        String str3 = String.format("%f", 101.00);       // Float value
+//        String str4 = String.format("%x", 101);          // Hexadecimal value
+//        String str5 = String.format("%c", 'c');          // Char value
+//        System.out.println(str1);
+//        System.out.println(str2);
+//        System.out.println(str3);
+//        System.out.println(str4);
+//        System.out.println(str5);
+//        System.out.println(military);
     }
 }
--- javaprograms/src/main/java/com/example/javaprograms/Tree.java
+++ javaprograms/src/main/java/com/example/javaprograms/Tree.java
@@ -1,6 +1,7 @@
 package com.example.javaprograms;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Tree {
@@ -105,6 +106,185 @@ public class Tree {
         System.out.println(node.value);
     }
 
+    //Height of the tree
+    public int height(){
+        return height(root);
+    }
+    private int height(Node root){
+        if(root == null)
+            return -1;
+        if(root.leftChild == null && root.rightChild == null)
+            return 0;
+        return 1 + Math.max(height(root.leftChild), height(root.rightChild));
+    }
+
+    //Find minimum of binary tree
+    public int findMin(){
+        return findMin(root);
+    }
+    public int findMin(Node root){
+        if(root.leftChild == null && root.rightChild == null)
+            return  root.value;
+        int left = findMin(root.leftChild);
+        int right = findMin(root.rightChild);
+
+        return Math.min((Math.min(left, right)), root.value);
+    }
+
+    //Finding minimum of binary search tree
+    public int bTSMin(){
+        if (root == null)
+            throw new IllegalStateException();
+
+        Node current = root;
+        Node last = current;
+        while (current != null){
+            last = current;
+            current = current.leftChild;
+        }
+
+        return last.value;
+    }
+
+    //Checking if two trees are the same
+    public boolean equals(Tree other){
+        return equals(root, other.root);
+    }
+    private boolean equals (Node first, Node second){
+        if(first == null && second == null)
+            return true;
+        if(first != null && second != null)
+            return first.value == second.value
+                    && equals(first.leftChild, second.leftChild)
+                    && equals(first.rightChild, second.rightChild);
+        return false;
+    }
+
+    //Validating a binary search tree
+    public boolean isBinarySearchTree(){
+        return isBinarySearchTree(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    private boolean isBinarySearchTree(Node root, int min, int max){
+        if(root == null)
+            return true;
+        if(root.value < min || root.value > max)
+            return false;
+        return isBinarySearchTree(root.leftChild, min, root.value - 1)
+                && isBinarySearchTree(root.rightChild, root.value + 1, max);
+    }
+
+    //Swapping nodes to make the tree not a binary search
+    public void swapRoot(){
+        Node temp = root.leftChild;
+        root.leftChild = root.rightChild;
+        root.rightChild = temp;
+    }
+
+    //Nodes at K distance from the root
+    public ArrayList<Integer> getNodesAtDistance(int distance){
+        ArrayList<Integer> list = new ArrayList<>();
+        getNodesAtDistance(root, distance, list);
+        return list;
+    }
+    private void getNodesAtDistance(Node root, int distance, ArrayList<Integer> list){
+        if(root == null)
+            return;
+        if(distance == 0){
+//            System.out.println(root.value);
+            list.add(root.value);
+            return;
+        }
+
+        getNodesAtDistance(root.leftChild, distance - 1, list);
+        getNodesAtDistance(root.rightChild, distance - 1, list);
+    }
+
+    //Finding number of leaves in tree
+    public int countLeaves(){
+        return countLeaves(root);
+    }
+    private int count = 0;
+    private int countLeaves(Node root){
+        if (root == null){
+            return 0;
+        }
+        else {
+            if (root.leftChild == null && root.rightChild == null){
+                count++;
+            }
+            else {
+                if (root.leftChild != null){
+                    countLeaves(root.leftChild);
+                }
+                if(root.rightChild != null){
+                    countLeaves(root.rightChild);
+                }
+            }
+        }
+        return count;
+    }
+
+    //Finding Max Value
+    public int max(){
+       return max(root);
+    }
+    private int max(Node root){
+        if (root.leftChild == null && root.rightChild == null)
+            return root.value;
+        int left = max(root.leftChild);
+        int right = max(root.rightChild);
+
+        return Math.max(Math.max(left, right), root.value);
+    }
+
+    //Max for Binary Search tree
+    public int BTSMax(){
+        Node current = root;
+        Node last = current;
+        while (current != null){
+            last = current;
+            current = current.rightChild;
+        }
+        return last.value;
+    }
+
+    //Find using recursion
+//    public boolean contains(int num){
+//
+//    }
+//    private boolean contains(Node root, int num){
+//        if (root == null)
+//            return false;
+//        if (root.value == num)
+//            return true;
+//
+//    }
+
+    //returning ancestors
+    public boolean getAncestors(int val){
+        return getAncestors(root, val);
+    }
+    private boolean getAncestors(Node root, int target)
+    {
+        /* base cases */
+        if (root == null)
+            return false;
+
+        if (root.value == target)
+            return true;
+
+        /* If target is present in either left or right subtree
+           of this node, then print this node */
+        if (getAncestors(root.leftChild, target)
+                || getAncestors(root.rightChild, target))
+        {
+            System.out.print(root.value + " ");
+            return true;
+        }
+
+        /* Else return false */
+        return false;
+    }
+
     //Find random node
-    
 }
