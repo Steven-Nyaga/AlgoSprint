9e49fbecb97b926e01fdbd45fc9fac113b690da9
Steven-Nyaga <37731180+Steven-Nyaga@users.noreply.github.com>
2021-07-08 05:21:37 +0300

    Trie implementation with HashMap

--- javaprograms/src/main/java/com/example/javaprograms/Trie.java
+++ javaprograms/src/main/java/com/example/javaprograms/Trie.java
@@ -1,11 +1,13 @@
 package com.example.javaprograms;
 
+import java.util.HashMap;
+
 public class Trie {
     public static int ALPHABET_SIZE = 26;
 
     private class Node {
         private char value;
-        private Node[] children = new Node[ALPHABET_SIZE];
+        private HashMap<Character, Node> children = new HashMap<>();
         private boolean isEndWord;
 
         public Node(char value) {
@@ -16,6 +18,18 @@ public class Trie {
         public String toString() {
             return "value=" + value;
         }
+
+        public boolean hasChild(char ch) {
+            return children.containsKey(ch);
+        }
+
+        public void addChild(char ch) {
+            children.put(ch, new Node(ch));
+        }
+
+        public Node getChild(char ch) {
+            return children.get(ch);
+        }
     }
 
     Node root = new Node(' ');
@@ -23,10 +37,9 @@ public class Trie {
     public void insert(String word){
         Node current = root;
         for (char ch : word.toCharArray()) {
-            int index = ch - 'a';
-            if (current.children[index] == null)
-                current.children[index] = new Node(ch);
-            current = current.children[index];
+            if (!current.hasChild(ch))
+                current.addChild(ch);
+            current = current.getChild(ch);
         }
         current.isEndWord = true;
     }
